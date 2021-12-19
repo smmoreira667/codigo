0f7006bd42febf79bd0f9ea579e7243dfd5adc7e
smmoreira667 <96272631+smmoreira667@users.noreply.github.com>
2021-12-19 00:26:08 -0300

    Create lm.sh

---
+++ lm.sh
@@ -0,0 +1,13 @@
+	giftcode = input("Please insert the GiftCode:")
+	player=["SrSteck"]
+
+    print(player)
+    time.sleep(1.5)
+    
+    #Checking if the player exists
+    playerCheck = requests.post('https://lordsmobile.igg.com/project/gifts/ajax.php?game_id=1051029902', data={'ac':'get_extra_info', 'charname': player, 'lang': 'en'})
+    print(playerCheck.text)
+
+    #Submitting the gift code
+    submitting = requests.post('https://lordsmobile.igg.com/project/gifts/ajax.php?game_id=1051029902', data={'ac':'get_gifts', 'type':'1', 'iggid':'0', 'charname': player, 'cdkey': giftcode, 'lang': 'en'})
+    print(submitting.text)
